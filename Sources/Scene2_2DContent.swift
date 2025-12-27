85faa69b86877d0c23250e2dfa8e80dd9b4b806c
kaseken <kent.kaseda.dev@gmail.com>
2025-12-27 20:21:34 +0900

    feat: Compile fragment shader

--- Sources/Scene2_2DContent.swift
+++ Sources/Scene2_2DContent.swift
@@ -48,7 +48,26 @@ func runScene2() {
         return
     }
 
-    // TODO: Compile fragment shader
+    // Step 3: Create and compile fragment shader
+    let FRAGMENT_SHADER: Int32 = 0x8B30
+    let fragmentShader = gl.createShader(FRAGMENT_SHADER)
+    _ = console.log("Step 3: Created fragment shader:", fragmentShader)
+
+    _ = gl.shaderSource(fragmentShader, fsSource)
+    _ = gl.compileShader(fragmentShader)
+    _ = console.log("Step 3: Compiled fragment shader")
+
+    // Check if compilation was successful
+    let fCompiled = gl.getShaderParameter(fragmentShader, COMPILE_STATUS)
+    _ = console.log("Step 3: Fragment shader compiled successfully?", fCompiled)
+
+    guard let fCompiledBool = fCompiled.boolean, fCompiledBool else {
+        let info = gl.getShaderInfoLog(fragmentShader)
+        _ = console.error("Fragment shader compilation failed:", info)
+        return
+    }
+
+    // TODO: Link shader program
     _ = gl.clearColor(0.2, 0.2, 0.3, 1.0)
     let COLOR_BUFFER_BIT: Int32 = 0x0000_4000
     _ = gl.clear(COLOR_BUFFER_BIT)
