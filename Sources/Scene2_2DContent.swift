c48e4561c6473fd7a7f1f7f19838ea443e465395
kaseken <kent.kaseda.dev@gmail.com>
2025-12-27 20:15:36 +0900

    fix: Add shader sources

--- Sources/Scene2_2DContent.swift
+++ Sources/Scene2_2DContent.swift
@@ -5,11 +5,32 @@ import JavaScriptKit
 
 @MainActor
 func runScene2() {
+    let console = JSObject.global.console
     let document = JSObject.global.document
     let canvasElement = document.getElementById("canvas-scene2")
     let gl = canvasElement.getContext("webgl")
 
-    // TODO: Implement shaders, buffers, and rendering
+    // Step 1: Define shader sources
+    let vsSource = """
+    attribute vec4 aVertexPosition;
+    uniform mat4 uModelViewMatrix;
+    uniform mat4 uProjectionMatrix;
+    void main() {
+        gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+    }
+    """
+
+    let fsSource = """
+    void main() {
+        gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+    """
+
+    _ = console.log("Step 1: Shader sources defined")
+    _ = console.log("Vertex shader:", vsSource)
+    _ = console.log("Fragment shader:", fsSource)
+
+    // TODO: Compile shaders
     _ = gl.clearColor(0.2, 0.2, 0.3, 1.0)
     let COLOR_BUFFER_BIT: Int32 = 0x0000_4000
     _ = gl.clear(COLOR_BUFFER_BIT)
